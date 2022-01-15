61c41760f21520203f85598e3b90825ce70d11ee
Enes Ozturk <enesozturk.d@gmail.com>
2022-01-15 21:32:25 +0300

    refactor: add gesture handler provider to root

--- App.tsx
+++ App.tsx
@@ -5,16 +5,19 @@ import Wallpaper from "@react-native-ios/components/Wallpaper";
 import Footer from "@react-native-ios/components/Footer/Footer";
 import Home from "@react-native-ios/screens/Home";
 import SwipeableProvider from "@react-native-ios/components/SwipeableProvider";
+import { GestureHandlerRootView } from "react-native-gesture-handler";
 
 export default function App() {
   return (
-    <SafeAreaProvider>
-      <StatusBar style="light" />
-      <Wallpaper />
-      <SwipeableProvider>
-        <Home />
-      </SwipeableProvider>
-      <Footer />
-    </SafeAreaProvider>
+    <GestureHandlerRootView>
+      <SafeAreaProvider>
+        <StatusBar style="light" />
+        <Wallpaper />
+        <SwipeableProvider>
+          <Home />
+        </SwipeableProvider>
+        <Footer />
+      </SafeAreaProvider>
+    </GestureHandlerRootView>
   );
 }
